ce5cf60dfcf79ab008fcaf4880d83f8443e6e943
lillianlakes <lillian.lakes@gmail.com>
2021-08-02 17:35:49 -0700

    adds some GET routes for users

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,24 @@
 const Router = require("express").Router;
 const router = new Router();
 
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
 
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
+
+router.get("/", ensureLoggedIn, async function (req, res, next) {
+
+  const result = await db.query(
+    `SELECT username, first_name, last_name, phone
+      FROM users`);
+  
+  let users = result.rows;
+
+  return res.json({ users });
+})
 
 
 /** GET /:username - get detail of users.
@@ -17,6 +29,18 @@ const router = new Router();
  *
  **/
 
+router.get("/:username", ensureCorrectUser, async function (req, res, next) {
+
+  const result = await db.query(
+  `SELECT username, first_name, last_name, phone, join_at, last_login_at
+   FROM users
+   WHERE username = $1`,
+   [req.params.username]);
+
+   let user = result.rows[0];
+
+   return res.json( { user });
+})
 
 /** GET /:username/to - get messages to user
  *
